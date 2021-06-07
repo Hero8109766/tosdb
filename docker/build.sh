65bfe0cad540c52da0f224a46ecb5de5fbd477b2
ebisuke <fungi@rect.sakura.ne.jp>
2021-06-07 22:01:51 +0900

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -28,9 +28,9 @@ do
     npm install
     npm run main ${region}
     # ->unzip
-    cd ${BASEDIR}
+    cd ${BASEDIR}/tos-web/dist/
     echo ${region,,}.zip
-    if [ $(unzip -o ./tos-build/dist/${region,,}.zip) -ge 2 ];then
+    if [ $(unzip -o ${region,,}.zip) -ge 2 ];then
         exit 1
     fi
     echo "complete"
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eu
+mkdir ./tos_build/dist | true
+cp -rf ./skeleton_distbuild ./tos_build/dist/
+mkdir ./tos_web/dist | true
+cp -rf ./skeleton_distweb ./tos_web/dist/
 
 /bin/bash /var/www/base/build.sh
-
 /bin/bash /var/www/base/bootstrap.sh
 
 echo "nginx READY!"
