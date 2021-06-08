774d0849476c0db1f62d7346584f8201e28fda15
ebisuke <fungi@rect.sakura.ne.jp>
2021-06-09 01:08:19 +0900

    fix nginx

--- docker/entrypoint_dev.sh
+++ docker/entrypoint_dev.sh
@@ -3,10 +3,10 @@ set -eu
 cd /var/www/base/
 BASEDIR=$(cd $(dirname $0); pwd)
 cd ${BASEDIR}
-#mkdir ./tos-build/dist | true
-#cp -rf ./skeleton_distbuild/* ./tos-build/dist/
-#mkdir ./tos-web/dist | true
-#cp -rf ./skeleton_distweb/* ./tos-web/dist/
+mkdir ./tos-build/dist | true
+cp -rf ./skeleton_distbuild/* ./tos-build/dist/
+mkdir ./tos-web/dist | true
+cp -rf ./skeleton_distweb/* ./tos-web/dist/
 
 
 /bin/bash ${BASEDIR}/bootstrap.sh
