b732eb844c6d1d8671c24f1c9441b83dcc02534b
ebisuke <fungi@rect.sakura.ne.jp>
2021-06-08 09:35:13 +0900

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -eu
 cd /var/www/base/
+BASEDIR=$(cd $(dirname $0); pwd)
+cd ${BASEDIR}
 mkdir ./tos-build/dist | true
 cp -rf ./skeleton_distbuild/* ./tos-build/dist/
 mkdir ./tos-web/dist | true
 cp -rf ./skeleton_distweb/* ./tos-web/dist/
 
 
-/bin/bash /var/www/base/build.sh
-/bin/bash /var/www/base/bootstrap.sh
+/bin/bash ${BASEDIR}/build.sh
+/bin/bash ${BASEDIR}/bootstrap.sh
 
 echo "nginx READY!"
 /usr/sbin/nginx
--- docker/entrypoint_dev.sh
+++ docker/entrypoint_dev.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -eu
-
 cd /var/www/base/
+BASEDIR=$(cd $(dirname $0); pwd)
+cd ${BASEDIR}
 mkdir ./tos-build/dist | true
 cp -rf ./skeleton_distbuild/* ./tos-build/dist/
 mkdir ./tos-web/dist | true
 cp -rf ./skeleton_distweb/* ./tos-web/dist/
 
 
-/bin/bash /var/www/base/bootstrap.sh
+/bin/bash ${BASEDIR}/bootstrap.sh
 
 echo "nginx READY!"
 /usr/sbin/nginx 
-BASEDIR=$(cd $(dirname $0); pwd)
+
 
 cd ${BASEDIR}/tos-web-rest/
 npm install 
