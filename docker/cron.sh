b31227a1d295b2fce9ea03884273840634ea1c1c
ebisuke <fungi@rect.sakura.ne.jp>
2021-06-30 14:41:23 +0900

    fix cron

--- docker/cron.sh
+++ docker/cron.sh
@@ -15,10 +15,10 @@ python3 main.py iTOS en 0 0
 
 parallel --no-notice --ungroup --colsep ' ' python3 main.py {1} {2} ${REPATCH} 0 :::: ../.././injectionlist_representative_without_itosktos.tsv 
 
-python3 main.py iTOS pt 0 0
-python3 main.py iTOS de 0 0
-python3 main.py iTOS th 0 0 
-python3 main.py iTOS ru 0 0
+python3 main.py iTOS pt 0 1
+python3 main.py iTOS de 0 1
+python3 main.py iTOS th 0 1 
+python3 main.py iTOS ru 0 1
 
 # search
 cd ${BASEDIR}/tos-search/
